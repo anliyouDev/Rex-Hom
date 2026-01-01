a8da128b033c20f769c3c10f1ba178524131cb4b
anliyouDev <massomaanliyoufofana@gmail.com>
2026-01-01 23:24:02 +0100

    Description de vos modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./Compenents/Navbar/Navbar";
-import Acceuil from "./Compenents/Acceuil/Acceuil";
-import Produits from "./Compenents/Produits/Produits";
-import Nosvaleurs from "./Compenents/Nosvaleurs/Nosvaleurs";
-import Contact from "./Compenents/Contacts/Contact";
-import Footer from "./Compenents/footer/Footer";
-import "./App.scss";
+import "./App.scss"
+import Acceuil from "./Compenents/Acceuil/Acceuil"
+import Navbar from "./Compenents/Navbar/Navbar"
+import Nosvaleurs from "./Compenents/Nosvaleurs/Nosvaleurs"
+import Produits from "./Compenents/Produits/Produits"
+import Contact from "./Compenents/Contacts/Contact" 
+import Footer from "./Compenents/footer/Footer"
 
 function App() {
+  
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Acceuil />} />
-        <Route path="/produits" element={<Produits />} />
-        <Route path="/valeurs" element={<Nosvaleurs />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    <>
+      <div className="App">
+        <section className="conteneur-acceuil">
+          <div className="acceuil">
+            <Navbar/>
+            <Acceuil/>
+          </div>
+        </section>
+        <section className="conteneur-produits">
+          <div className="produits">
+            <Produits/>
+          </div>
+        </section>
+        <section className="conteneur-valeurs">
+          <div className="nos-valeurs">
+             <Nosvaleurs/>
+          </div>
+        </section>
+        <section className="conteneur-contact">
+          <div className="contact">
+            <Contact/>
+          </div>
+        </section>
+        <section className="conteneur-footer">
+          <div className="footer">
+            <Footer/>
+          </div>
+        </section>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/Compenents/Navbar/Navbar.jsx
+++ src/Compenents/Navbar/Navbar.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import "./Navbar.scss"
-import { FiMenu } from "react-icons/fi"
-import { Link } from "react-router-dom"
-
+import { FiMenu } from "react-icons/fi";
 const Navbar = () => {
   return (
     <div className='Navbar'>
-      <div className="laptop">
-        <h2 className="logo">RexHom</h2>
-        <div className="navigation">
-          <ul className="links-conteneur">
-            <li className="link"><Link to="/">Résidence</Link></li>
-            <li className="link"><Link to="/valeurs">Nos valeurs</Link></li>
-            <li className="link"><Link to="/contact">Contact</Link></li>
-            <li className="link"><Link to="/cestparti">C'est parti</Link></li>
-            <li className="link"><Link to="/louer">Louer</Link></li>
-          </ul>
-          <FiMenu className='menu-icon' />
+        <div className="laptop">
+            <h2 className="logo">RexHom</h2>
+            <div className="navigation">
+                <ul className="links-conteneur">
+                    <li className="link">Résidence</li>
+                    <li className="link">Nos valeurs</li>
+                    <li className="link">Contact</li>
+                    <li className="link">C'est parti</li>
+                    <li className="link">Louer</li>
+                </ul>
+                <FiMenu  className='menu-icon'/>
+            </div>
         </div>
-      </div>
-      <div className="mobile"></div>
+        <div className="mobile"></div>
     </div>
   )
 }
